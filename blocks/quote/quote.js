e493de220f6782590a0c370626be539d69234df8
sagor-de <sachowdhuri@deloitte.com>
2025-04-09 13:34:25 +0530

    Updated quote block

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,3 +1,5 @@
+import utils from "../../utils/utils.js";
+
 export default function decorate(block) {
     const quoteWrapper = block.children;
     console.log(quoteWrapper[1])
@@ -5,4 +7,14 @@ export default function decorate(block) {
     const blockquote = document.createElement('blockquote');
     blockquote.textContent = quoteWrapper.textContent.trim();
     quoteWrapper.replaceChildren(blockquote);
+
+ block.innerHTML = utils.sanitizeHtml(`
+    <div class="quote-wrapper">
+    <div class="blockquote">
+      <h1>Intuitive design happens when <span>current knowledge</span> is the same as the <span>target knowledge.</span></h1>
+      <h4>&mdash;Prosenjit Sikdar<br><em>Web Site Usability: A Designer's Guide</em></h4>
+    </div>
+  </div>`);
+
+
   }
---
+++ utils/utils.js
@@ -0,0 +1,9 @@
+const utils = {
+    sanitizeHtml(html) {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      return doc.body.innerHTML;
+    },
+  };
+  
+  export default utils;
