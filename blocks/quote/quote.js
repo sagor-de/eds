086b3560e88d33075e9d24ca8c2c527c8a77283d
sagor-de <sachowdhuri@deloitte.com>
2025-04-09 11:12:21 +0530

    Updated quote block

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
-    const [quoteWrapper] = block.children;
-  
+    const quoteWrapper = block.children;
+    console.log(quoteWrapper[1])
+    debugger;
     const blockquote = document.createElement('blockquote');
     blockquote.textContent = quoteWrapper.textContent.trim();
     quoteWrapper.replaceChildren(blockquote);
