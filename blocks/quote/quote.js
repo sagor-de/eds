a3bbd06e494e8214a6762ab3e9ced5b0f52859a5
sagor-de <sachowdhuri@deloitte.com>
2025-04-09 17:02:20 +0530

    Updated quote block

---
+++ blocks/quote/quote.js
@@ -0,0 +1,18 @@
+export default function decorate(block) {
+    const [
+        titleEl,
+        authorEl,
+        bgcolorEl,
+    ] = block.children;
+    console.log(titleEl);
+    const title = titleEl.querySelector('p').innerText;
+    const author = authorEl.querySelector('p').innerText;
+    const bgcolor = bgcolorEl.querySelector('p').innerText;
+    block.innerHTML = `<div class = "${bgcolor}">
+        <div class="blockquote">
+        <h1>${title}</h1>
+        <h4>&mdash;${author}<br><em>Web Site Usability: A Designer's Guide</em></h4>
+        </div>
+    </div>`;
+
+}
