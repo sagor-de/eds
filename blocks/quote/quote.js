66322b2d0d975775d662551a7dd21cecedd3b863
sagor-de <sachowdhuri@deloitte.com>
2025-04-09 16:49:40 +0530

    Updated quote block

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,17 +1,16 @@
+import utils from "../../utils/utils.js";
+
 export default function decorate(block) {
-    const [
-      titleEl,
-      authorEl,
-      bgcolorEl,
-    ] = block.children;
-    console.log(titleEl);
-    const title = titleEl.querySelector('p').innerText;
-    const author = authorEl.querySelector('p').innerText;
-    const bgcolor = bgcolorEl.querySelector('p').innerText;
-    block.innerHTML = `<div class = "${bgcolor}">
-        <div class="blockquote">
-          <h1>${title}</h1>
-          <h4>&mdash;${author}<br><em>Web Site Usability: A Designer's Guide</em></h4>
-        </div>
-      </div>`;
+    const quoteWrapper = block.children;
+    console.log(quoteWrapper[1])
+
+ block.innerHTML = `
+    <div class="quote-wrapper">
+    <div class="blockquote">
+      <h1>Intuitive design happens when <span>current knowledge</span> is the same as the <span>target knowledge.</span></h1>
+      <h4>&mdash;Prosenjit Sikdar<br><em>Web Site Usability: A Designer's Guide</em></h4>
+    </div>
+  </div>`
+
+
   }
