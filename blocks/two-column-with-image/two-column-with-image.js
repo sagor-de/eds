5a1af4f3f9eaf61b92a89850112b6d0f2ad3cd2b
sagor-de <sachowdhuri@deloitte.com>
2025-04-18 11:30:28 +0530

    Updated additional links

--- blocks/two-column-with-image/two-column-with-image.js
+++ blocks/two-column-with-image/two-column-with-image.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
     const childElements = block.children;
     console.log('childElements', childElements);
-    debugger;
+    console.log('This is Main Branch ====================================');
     const placement = childElements[0].querySelector('p') ? childElements[0].querySelector('p').textContent : '';
     const image = childElements[1].querySelector('img') ? childElements[1].querySelector('img') : '';
     const imageAlt = childElements[2];
